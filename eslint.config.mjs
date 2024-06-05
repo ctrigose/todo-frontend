3fcabc6d245fc451c03a2119e926eb4a0f85e6b8
Carlos Trigo Seguin <37633966+ctrigose@users.noreply.github.com>
2024-06-05 16:10:53 +0200

    Add github action: build and lint checks when merging into main (#2)
    * build check action on pull_request main
    
    * enable corepack in action

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,10 +3,17 @@ import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
 
-
-export default [
-  {languageOptions: { globals: globals.browser }},
+const config = [
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReactConfig,
+  {
+    rules: {
+      // suppress errors for missing 'import React' in files
+      "react/react-in-jsx-scope": "off",
+    },
+  },
 ];
+
+export default config;
